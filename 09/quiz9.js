ca092abea4c1a4f10e59088974d1c824cf9bfbb1
lee-hyunsu <dlgustn675@naver.com>
2022-05-12 15:59:44 +0900

    quiz9 수정

--- 09/quiz9.js
+++ 09/quiz9.js
@@ -1,3 +1,28 @@
 const input = document.querySelector('#url')
 const btn = document.querySelector('#btn')
 const log = document.querySelector('#log')
+
+function fetchUrl() {
+    const inputValue = document.querySelector('#url').value
+    fetch(inputValue)
+        .then(resData)
+        .catch(err => (log.value = err.message))
+}
+
+const resData = async res => {
+    log.value = await res.text()
+}
+
+function enterPress(e) {
+    if (e.key === 'Enter') {
+        e.preventDefault()
+        btn.click()
+    }
+}
+
+function init() {
+    input.addEventListener('keypress', enterPress)
+    btn.addEventListener('click', fetchUrl)
+}
+
+init()
